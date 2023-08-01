e43cf4a23d7623cc9af0ba46106164eb172415da
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-02 02:02:14 +0600

    some changes in themes

--- src/lib/data/themes.ts
+++ src/lib/data/themes.ts
@@ -1,18 +1,16 @@
 const themes = {
   light: [
+    { name: 'GitHub', value: 'github' },
+    { name: 'Chrome', value: 'chrome' },
     { name: 'Xcode', value: 'xcode' },
-    { name: 'Textmate', value: 'textmate' },
-    { name: 'Kuroir', value: 'kuroir' },
-    { name: 'Crimson', value: 'crimson_editor' },
-    { name: 'Solarized', value: 'solarized_light' },
-    { name: 'Github Light', value: 'github' },
+    { name: 'Solarized Light', value: 'solarized_light' },
   ],
   dark: [
+    { name: 'GitHub Dark', value: 'github_dark' },
     { name: 'Monokai', value: 'monokai' },
-    { name: 'Gob', value: 'gob' },
+    { name: 'Dracula', value: 'dracula' },
     { name: 'Cobalt', value: 'cobalt' },
-    { name: 'Merbivore Soft', value: 'merbivore_soft' },
-    { name: 'Github Dark', value: 'github_dark' },
+    { name: 'Gob', value: 'gob' },
   ],
 };
 
--- src/pages/snippets/Create.tsx
+++ src/pages/snippets/Create.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 
@@ -8,43 +8,61 @@ import themes from '../../lib/data/themes';
 
 import AceEditor from 'react-ace';
 
-import 'ace-builds/src-noconflict/mode-javascript';
-
-import 'ace-builds/src-noconflict/theme-cobalt';
-import 'ace-builds/src-noconflict/theme-crimson_editor';
-import 'ace-builds/src-noconflict/theme-github';
-import 'ace-builds/src-noconflict/theme-github_dark';
-import 'ace-builds/src-noconflict/theme-gob';
-import 'ace-builds/src-noconflict/theme-kuroir';
-import 'ace-builds/src-noconflict/theme-merbivore_soft';
-import 'ace-builds/src-noconflict/theme-monokai';
-import 'ace-builds/src-noconflict/theme-solarized_light';
-import 'ace-builds/src-noconflict/theme-textmate';
-import 'ace-builds/src-noconflict/theme-xcode';
+import '../../utils/imports/ace-languages';
+import '../../utils/imports/ace-themes';
 
 import 'ace-builds/src-noconflict/ext-language_tools';
 
-const Create = () => {
-  const pending = useRef(false);
-  const fontSizes = [14, 16, 18, 20, 22, 24];
+type Language = {
+  ext: string;
+  name: string;
+  mode: string;
+};
 
-  const [formData, setFormData] = useState({
+type formDataType = {
+  title: string;
+  language: string;
+  source_code: string;
+  font_size: number;
+  theme: string;
+  visibility: undefined | number;
+  pass_code: undefined | string;
+  _tags: string;
+};
+
+const Create = () => {
+  const [pending, setPending] = useState<boolean>(false);
+  const fontSizes: number[] = [14, 16, 18, 20, 22, 24];
+  const initialFormData: formDataType = {
     title: '',
-    language: 'cpp',
+    language: '',
+    source_code: '',
     font_size: 18,
     theme: 'monokai',
-    sourceCode: '',
+    visibility: undefined,
+    pass_code: undefined,
     _tags: '',
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [mode, setMode] = useState<string>('');
+
+  const handleLanguageChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = event.target.options[event.target.selectedIndex];
+    const selectedMode = selectedOption.getAttribute('data-mode');
+    if (selectedMode) {
+      setMode(selectedMode);
+    }
+  };
 
   return (
-    <div className="mx-auto py-7 px-5 max-w-3xl">
+    <div className="mx-auto py-7 px-5 max-w-5xl">
       <div className="flex justify-between items-start mb-5 border-b-4 border-gray-700">
         <h1 className="text-2xl md:text-3xl font-bold mb-4">
           Create a Snippet
         </h1>
         <Link
-          className="px-3 py-1 text-white rounded bg-black hover:bg-gray-600"
+          className="px-2 py-1 text-white rounded bg-black hover:bg-gray-600"
           to="/"
         >
           Back
@@ -66,19 +84,71 @@ const Create = () => {
       />
 
       <form>
-        <div className="mb-4">
-          <label htmlFor="title" className="block mb-1 font-semibold">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={formData.title}
-            placeholder="Enter title"
-            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
-          />
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-5">
+          <div className="mb-4">
+            <label htmlFor="title" className="block mb-1 font-semibold">
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={formData.title}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  title: e.target.value,
+                })
+              }
+              placeholder="Enter title"
+              className="w-full px-2 py-1 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="visibility" className="block mb-1 font-semibold">
+              Visibility
+            </label>
+            <select
+              name="visibility"
+              id="visibility"
+              className="w-full px-2 py-[6px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
+              value={formData.visibility}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  visibility: parseInt(e.target.value),
+                })
+              }
+            >
+              <option value="">Select a font size</option>
+              <option value="1">Public</option>
+              <option value="2">Private</option>
+            </select>
+          </div>
         </div>
+
+        {formData.visibility === 2 && (
+          <div className="mb-4">
+            <label htmlFor="title" className="block mb-1 font-semibold">
+              Passcode
+            </label>
+            <input
+              type="text"
+              name="pass_code"
+              id="pass_code"
+              value={formData.pass_code}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  pass_code: e.target.value,
+                })
+              }
+              placeholder="Enter passcode"
+              className="w-full px-2 py-1 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
+            />
+          </div>
+        )}
 
         <div className="mb-4">
           <label htmlFor="language" className="block mb-1 font-semibold">
@@ -87,68 +157,74 @@ const Create = () => {
           <select
             name="language"
             id="language"
-            className="w-full px-3 py-[10px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
+            className="w-full px-2 py-[6px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
+            onChange={handleLanguageChange}
           >
             <option value="">Select a language</option>
-            {languages.map((lang, index) => (
-              <option key={index} value={lang.ext}>
+            {languages.map((lang: Language, index: number) => (
+              <option key={index} value={lang.ext} data-mode={lang.mode}>
                 {lang.name}
               </option>
             ))}
           </select>
         </div>
 
-        <div className="mb-4">
-          <label htmlFor="font_size" className="block mb-1 font-semibold">
-            Font Size
-          </label>
-          <select
-            name="font_size"
-            id="font_size"
-            className="w-full px-3 py-[10px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
-            value={formData.font_size}
-            onChange={(e) =>
-              setFormData({ ...formData, font_size: parseInt(e.target.value) })
-            }
-          >
-            <option value="">Select a font size</option>
-            {fontSizes.map((size, index) => (
-              <option key={index} value={size}>
-                {size}
-              </option>
-            ))}
-          </select>
-        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-5">
+          <div className="mb-4">
+            <label htmlFor="font_size" className="block mb-1 font-semibold">
+              Font Size
+            </label>
+            <select
+              name="font_size"
+              id="font_size"
+              className="w-full px-2 py-[6px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
+              value={formData.font_size}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  font_size: parseInt(e.target.value),
+                })
+              }
+            >
+              <option value="">Select a font size</option>
+              {fontSizes.map((size, index) => (
+                <option key={index} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="theme" className="block mb-1 font-semibold">
-            Theme
-          </label>
-          <select
-            name="theme"
-            id="theme"
-            className="w-full px-3 py-[10px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
-            value={formData.theme}
-            onChange={(e) =>
-              setFormData({ ...formData, theme: e.target.value })
-            }
-          >
-            <option value="">Select a theme</option>
-            <optgroup label="Light">
-              {themes.light.map((theme, index) => (
-                <option key={index} value={theme.value}>
-                  {theme.name}
-                </option>
-              ))}
-            </optgroup>
-            <optgroup label="Dark">
-              {themes.dark.map((theme, index) => (
-                <option key={index} value={theme.value}>
-                  {theme.name}
-                </option>
-              ))}
-            </optgroup>
-          </select>
+          <div className="mb-4">
+            <label htmlFor="theme" className="block mb-1 font-semibold">
+              Theme
+            </label>
+            <select
+              name="theme"
+              id="theme"
+              className="w-full px-2 py-[6px] border-2 bg-white border-gray-300 rounded focus:outline-none focus:border-black"
+              value={formData.theme}
+              onChange={(e) =>
+                setFormData({ ...formData, theme: e.target.value })
+              }
+            >
+              <option value="">Select a theme</option>
+              <optgroup label="Light">
+                {themes.light.map((theme, index) => (
+                  <option key={index} value={theme.value}>
+                    {theme.name}
+                  </option>
+                ))}
+              </optgroup>
+              <optgroup label="Dark">
+                {themes.dark.map((theme, index) => (
+                  <option key={index} value={theme.value}>
+                    {theme.name}
+                  </option>
+                ))}
+              </optgroup>
+            </select>
+          </div>
         </div>
 
         <div className="mb-4">
@@ -156,13 +232,11 @@ const Create = () => {
             Source Code
           </label>
           <AceEditor
-            mode="javascript"
+            mode={mode}
             theme={formData.theme}
             width="100%"
+            height="800px"
             fontSize={formData.font_size}
-            // onChange={onChange}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{ $blockScrolling: true }}
           />
         </div>
 
@@ -174,14 +248,21 @@ const Create = () => {
           <input
             type="text"
             name="_tags"
+            value={formData._tags}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                _tags: e.target.value,
+              })
+            }
             placeholder="ex: javascript, react, nodejs"
-            className="w-full px-3 py-2 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
+            className="w-full px-2 py-1 border-2 border-gray-300 rounded focus:outline-none focus:border-black"
           />
         </div>
 
         <button
           type="submit"
-          className={`px-4 py-2 text-white rounded hover:bg-gray-600 ${
+          className={`px-4 py-1 text-white rounded hover:bg-gray-600 ${
             pending ? 'bg-gray-700' : 'bg-black '
           }`}
           disabled={pending.current}
