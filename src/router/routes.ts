56037f7c6ff06da7f24930e0104fe468472bda17
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-09 00:54:44 +0600

    JSX.Element instead of ReactNode make it works

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,16 +1,27 @@
-import { ReactNode } from 'react';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Me from '../pages/profile/Me';
+import Create from '../pages/snippets/Create';
+import Edit from '../pages/snippets/Edit';
+import Library from '../pages/snippets/Library';
+import Show from '../pages/snippets/Show';
 
 type RouteType = {
   path: string;
-  element: ReactNode;
-  isPrivate: undefined | boolean;
+  element: () => JSX.Element;
+  _protected: number;
 };
 
 const routes: RouteType[] = [
-  { path: '/', element: <Home /> },
-  { path: '/login', element: <Login />, isPrivate: false },
-  { path: '/register', element: <Register />, isPrivate: false },
-  { path: '/snippets/create', element: <Create />, isPrivate: true },
+  { path: '/', element: Home, _protected: -1 },
+  { path: '/login', element: Login, _protected: 0 },
+  { path: '/register', element: Register, _protected: 0 },
+  { path: '/me', element: Me, _protected: 1 },
+  { path: '/library', element: Library, _protected: 1 },
+  { path: '/snippets/create', element: Create, _protected: 1 },
+  { path: '/snippets/edit/:id', element: Edit, _protected: 1 },
+  { path: '/snippets/:id', element: Show, _protected: 0 },
 ];
 
 export default routes;
