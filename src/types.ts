c5237a5c3aa3bfb908cd647850efd5d4e9938b99
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-12 01:28:57 +0600

    all type definitions are declared in a single file

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export type LanguageType = {
+  ext: string;
+  name: string;
+  mode: string;
+};
+
+export type ThemeType = {
+  name: string;
+  value: string;
+  is_dark: boolean;
+};
+
+export type formDataType = {
+  title: string;
+  language: string;
+  source_code: string;
+  font_size: number;
+  theme: string;
+  visibility: undefined | number;
+  pass_code: undefined | string;
+  tags: string[];
+};
+
+export type AuthContextType = {
+  auth: {
+    name: string;
+    email: string;
+    token: string;
+  };
+  setAuth: (auth: { name: string; email: string; token: string }) => void;
+};
+
+export type RouteType = {
+  path: string;
+  element: () => JSX.Element;
+  _protected: number; // {-1: public, 0: shouldBeLoggedOut, 1: shouldBeLoggedIn}
+};
