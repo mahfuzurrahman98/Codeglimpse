78864b90908757a2201504a370d88b7432c1bfe0
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-15 13:13:58 +0600

    css change and route mapping

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,47 +32,47 @@ const Navbar: FC = () => {
   }, []);
 
   return (
-    <nav className=" bg-white shadow w-full flex relative justify-between items-center mx-auto px-8 h-16 max-w-[800px]">
-      <div className="inline-flex">
-        <Link className="" to="/">
-          <div className="">
-            <img src={Logo} alt="" className="w-8" />
-          </div>
-        </Link>
-      </div>
+    <div className="bg-white shadow">
+      <nav className="w-full flex justify-between items-center mx-auto px-8 h-16 max-w-5xl">
+        <div className="">
+          <Link className="" to="/">
+            <div className="">
+              <img src={Logo} alt="" className="w-8" />
+            </div>
+          </Link>
+        </div>
 
-      <div className="flex mr-4 items-center">
-        <Link
-          to="/create"
-          className="flex items-center gap-x-1 py-2 px-3 hover:bg-gray-200 rounded-full"
-        >
-          <span>
-            <img src={PlusIcon} className="w-5" alt="" />
-          </span>
-          <span>New</span>
-        </Link>
-      </div>
+        <div className="flex mr-4 items-center">
+          <Link
+            to="/p/new"
+            className="flex items-center gap-x-1 py-2 px-3 hover:bg-gray-200 rounded-full"
+          >
+            <span>
+              <img src={PlusIcon} className="w-5" alt="" />
+            </span>
+            <span>New</span>
+          </Link>
+        </div>
 
-      <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
-        <div className="inline-block">
-          <div className="items-center max-w-full b-red-500 relative">
-            <input
-              className="border-[3px] rounded-3xl border-black pl-4 py-1 pr-10"
-              placeholder="Start your search"
-            />
-            <img
-              src={SearchIcon}
-              className="w-5 absolute top-0 right-0 mt-2 mr-3 pointer-events-none"
-            />
+        <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
+          <div className="inline-block">
+            <div className="items-center max-w-full b-red-500 relative">
+              <input
+                className="border-[3px] rounded-3xl border-black pl-4 py-1 pr-10"
+                placeholder="Start your search"
+              />
+              <img
+                src={SearchIcon}
+                className="w-5 absolute top-0 right-0 mt-2 mr-3 pointer-events-none"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex-initial">
         <div className="flex justify-end items-center relative">
           <div className="flex mr-4 items-center">
             <Link
-              to="/library"
+              to="/p/library"
               className="flex items-center gap-x-1 py-2 px-3 hover:bg-gray-200 rounded-full"
             >
               <span>
@@ -96,15 +96,16 @@ const Navbar: FC = () => {
 
             {isDropdownOpen && (
               <div className="absolute right-0 mt-40 w-48 bg-white border rounded-lg shadow-lg">
-                <a
-                  href="#"
+                <Link
+                  to="/me"
                   className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
                 >
                   Profile
-                </a>
+                </Link>
                 <a
                   href="#"
                   className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                  onClick={() => _logout()}
                 >
                   Logout
                 </a>
@@ -112,8 +113,8 @@ const Navbar: FC = () => {
             )}
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
