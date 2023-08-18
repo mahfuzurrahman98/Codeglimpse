02671f9db7dff9a1ecbc227c29fc121b865bacad
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-18 20:42:51 +0600

    just made it work

--- src/pages/snippets/Show.tsx
+++ src/pages/snippets/Show.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from '../../api/axios';
+import { SnippetType } from '../../types';
+
 const Show = () => {
+  const [snippet, setSnippet] = useState<SnippetType | null>(null);
+  const params = useParams();
+  const uid = params.id;
+
+  useEffect(() => {
+    // self invoking function
+    (async () => {
+      try {
+        const response = await axios.get(`/snippets/${uid}`);
+        console.log(response.data.data.snippet);
+        setSnippet(response.data.data.snippet);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Show</h1>
+      <pre>
+        <code>{snippet?.source_code}</code>
+      </pre>
     </div>
   );
 };
