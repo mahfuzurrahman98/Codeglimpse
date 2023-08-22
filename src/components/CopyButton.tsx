0c586016257c487e7563f52dda2eb423e1f6aa50
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-22 19:01:28 +0600

    coppy button componenta added

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,36 @@
+import toast, { Toaster } from 'react-hot-toast';
+import ClipboardIcon from '../assets/clipboard.svg';
+
+const CopyButton = ({ sourceCode }: { sourceCode: string }) => {
+  return (
+    <>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+          },
+        }}
+      />
+
+      <button
+        className="font-semibold rounded-md px-2 py-1 bg-white flex gap-x-1 items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(sourceCode);
+          toast.success('Copied to clipboard!');
+        }}
+      >
+        <img src={ClipboardIcon} alt="copy" width={12} className="text-white" />
+        <span className="text-black text-xs">Copy</span>
+      </button>
+    </>
+  );
+};
+
+export default CopyButton;
