502d76a54d24d6a3fe43fcd8d5ab362e7371a023
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-09-01 12:17:31 +0600

    404 page designed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/terminal.svg';
+
 const NotFound = () => {
-  return <h1>404df</h1>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="rounded flex flex-col justify-center text-center">
+        <h2 className="text-3xl font-semibold mb-4">404 | Not found</h2>
+        <p className="text-gray-200 mb-4">
+          The page you are looking for is not valid.
+        </p>
+
+        <Link
+          to="/"
+          className="flex justify-center items-center gap-x-2 bg-white  py-1 text-black rounded"
+        >
+          <span>
+            <img src={Logo} className="w-4" alt="" />
+          </span>
+          <span>Back to Home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
