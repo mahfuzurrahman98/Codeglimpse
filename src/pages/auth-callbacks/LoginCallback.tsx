938c01c70e27f54a7bf7f5da90c0950b496771a4
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-09-04 23:12:05 +0600

    set error message on login callback

--- src/pages/auth-callbacks/LoginCallback.tsx
+++ src/pages/auth-callbacks/LoginCallback.tsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import axios from '../../api/axios';
 import Loading from '../../components/Loading';
 import useAuth from '../../hooks/useAuth';
+
+const ErrMsg = ({ errMsg }: { errMsg: string }) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded flex flex-col gap-y-3">
+        <div>
+          <p className="font-bold">Error!</p>
+          <p className="">{errMsg}</p>
+        </div>
+        <div>
+          <Link
+            className="px-4 py-1 text-white rounded hover:bg-gray-600 bg-black"
+            to={'/'}
+          >
+            Back to home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const LoginCallback = () => {
   const [searchParams] = useSearchParams();
@@ -27,7 +48,6 @@ const LoginCallback = () => {
         picture: user.picture,
         token: accessToken,
       });
-
       localStorage.setItem('picture', user.picture);
     } catch (error: any) {
       setErrMsg(error.message);
@@ -50,11 +70,7 @@ const LoginCallback = () => {
     })();
   }, []);
 
-  return (
-    <div>
-      <Loading />
-    </div>
-  );
+  return <>{errMsg ? <ErrMsg errMsg={errMsg} /> : <Loading />}</>;
 };
 
 export default LoginCallback;
