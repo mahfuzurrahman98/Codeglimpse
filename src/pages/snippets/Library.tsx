e87082500a14a8f9ca1d0dba4561eb38fcc399e5
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-09-04 22:29:54 +0600

    table loaded successfully with data

--- src/pages/snippets/Library.tsx
+++ src/pages/snippets/Library.tsx
@@ -3,28 +3,28 @@ import { useSearchParams } from 'react-router-dom';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 // import useAuth from '../../hooks/useAuth';
 import { SnippetType } from '../../types';
-import RootLayout from '../RootLayout';
 
 import '../../utils/imports/ace-languages';
 import '../../utils/imports/ace-themes';
 
 import 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/ext-modelist';
+import SnippetLayout from './SnippetLayout';
 
 const Home = () => {
   // const { auth } = useAuth();
   const [snippets, setSnippets] = useState<SnippetType[]>([]);
   const [searchParams] = useSearchParams();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
   // if search params change set snippets to new data
   const axiosPrivate = useAxiosPrivate();
@@ -54,89 +54,61 @@ const Home = () => {
   }, []);
 
   return (
-    /*
-    languages.find((language) => language.name === snippet.language)
-                ?.mode as string
-                */
-    <RootLayout>
-      <>
-        <div className="">
-          {isModalOpen && (
-            <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm">
-              <div className="bg-white w-96 p-6 rounded-lg shadow-lg">
-                <h2 className="text-xl font-bold mb-4">Modal Title</h2>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam a metus eu nulla dignissim malesuada.
-                </p>
-                <button
-                  className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
-                  onClick={closeModal}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="max-w-4xl mx-auto mt-5 flex flex-col gap-y-5 px-3 lg:px-0 ">
-          <div className="flex flex-col">
-            <div className="-m-1.5 overflow-x-auto">
-              <div className="p-1.5 min-w-full inline-block align-middle">
-                <div className="overflow-hidden">
-                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <thead>
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-md font-bold"
+    <SnippetLayout>
+      <div className="flex flex-col">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="overflow-hidden">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-md font-bold"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-md font-bold"
+                    >
+                      Language
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-right text-md font-bold"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                  {snippets.map((snippet: SnippetType, index: number) => (
+                    <tr key={index}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">
+                        {snippet.title}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm">
+                        {snippet.language}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-bold">
+                        <a
+                          // onClick={openModal}
+                          className="text-red-500 hover:text-blue-700"
+                          href="#"
                         >
-                          Title
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-md font-bold"
-                        >
-                          Language
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-right text-md font-bold"
-                        >
-                          Action
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                      {snippets.map((snippet: SnippetType, index: number) => (
-                        <tr key={index}>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">
-                            {snippet.title}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm">
-                            {snippet.language}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-bold">
-                            <a
-                              onClick={openModal}
-                              className="text-blue-500 hover:text-blue-700"
-                              href="#"
-                            >
-                              Delete
-                            </a>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
+                          Delete
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </>
-    </RootLayout>
+      </div>
+    </SnippetLayout>
   );
 };
 
