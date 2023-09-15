0e2e317d6e82311d14b6748c4ba43ad084a85849
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-09-15 17:59:08 +0600

    limit changed to 10

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ import SearchBox from '../components/SearchBox';
 
 type _SnippetType = SnippetType & { mode: string };
 
-const _limit = 1;
+const _limit = 10;
 
 export const Snippet = ({ snippet }: { snippet: _SnippetType }) => {
   return (
